15d66f647d3e59289d28cf594c7bfeaee4da22cf
Vinayak Sharma <vinayaksh42@gmail.com>
2020-08-07 21:40:38 +0530

    Updates

---
+++ precache-manifest.d835eb96d0a36ba167b7bc61b3bbca4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59558525db1f7dc508dfca7b7ca8c96f",
+    "url": "/Todoapp/index.html"
+  },
+  {
+    "revision": "07027527301cf66f996e",
+    "url": "/Todoapp/static/css/main.d03f01ea.chunk.css"
+  },
+  {
+    "revision": "fb88cffcfa32e9498106",
+    "url": "/Todoapp/static/js/2.d1e7220c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Todoapp/static/js/2.d1e7220c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07027527301cf66f996e",
+    "url": "/Todoapp/static/js/main.58f95e1b.chunk.js"
+  },
+  {
+    "revision": "83e594f563a5bf7d39e3",
+    "url": "/Todoapp/static/js/runtime-main.df4b4918.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c9a833f79708295df068f4853041a391.js"
+  "/Todoapp/precache-manifest.d835eb96d0a36ba167b7bc61b3bbca4a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Todoapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
